38da71c2dfac541226e5545dfce94ad5e12364c6
melalex <melalex490@gmail.com>
2017-01-31 16:35:50 +0200

    Task 2 javadoc comments

--- src/com/company/bitoperations/implementation/BitChanger.java
+++ src/com/company/bitoperations/implementation/BitChanger.java
@@ -1,20 +1,42 @@
 package com.company.bitoperations.implementation;
 
 /**
+ * Provides functionality to changing bit value of int number
+ *
  * Created by alexander on 31/01/17.
  */
 public class BitChanger {
 
-    public enum ChangeMode {ZERO, ONE}
+    /**
+     * Mode of changing
+     */
+    public enum ChangeMode {
+        /** set bit to 0*/
+        ZERO,
+        /** set bit to 1*/
+        ONE
+    }
 
-    public int changeBit(int number, int pos, ChangeMode mode) throws IllegalArgumentException {
+    /**
+     * Change in the variable pos bit by 1 or 0.
+     *
+     * @param number number that must be changed
+     * @param pos index of changing bit
+     * @param mode mode of changing:
+     *             ZERO - to 0
+     *             ONE - to 1
+     * @return changed number
+     * @throws IllegalArgumentException if pos not in diapason ({@code pos < 0 || pos >= Integer.SIZE})
+     * @see Integer#SIZE
+     */
+    public static int changeBit(int number, int pos, ChangeMode mode) throws IllegalArgumentException {
         if (pos > Integer.SIZE) {
             throw new IllegalArgumentException(String.format("Pos must be less than %d. Got %d",
                     Integer.SIZE, pos));
         }
 
         if (pos < 0) {
-            throw new IllegalArgumentException(String.format("Pos must be greater than %d. Got %d"
+            throw new IllegalArgumentException(String.format("Pos must be greater than or equal to %d. Got %d"
                     , 0, pos));
         }
 
