42aea256fbebea7378002103cc1c5d02256fe33d
melalex <melalex490@gmail.com>
2017-02-02 23:52:59 +0200

    task 1

---
+++ src/com/company/bitoperations/implementation/BitsCount.java
@@ -0,0 +1,54 @@
+package com.company.bitoperations.implementation;
+
+/**
+ * Created by alexander on 02/02/17.
+ */
+public class BitsCount {
+    public static int byteBitCount() {
+        byte number = ~0;
+        int count = 0;
+
+        while (number != 0) {
+            number >>>= 1;
+            count++;
+        }
+
+        return count;
+    }
+
+    public static int shortBitCount() {
+        short number = ~0;
+        int count = 0;
+
+        while (number != 0) {
+            number >>>= 1;
+            count++;
+        }
+
+        return count;
+    }
+
+    public static int intBitCount() {
+        int number = ~0;
+        int count = 0;
+
+        while (number != 0) {
+            number >>>= 1;
+            count++;
+        }
+
+        return count;
+    }
+
+    public static int longBitCount() {
+        long number = ~0L;
+        int count = 0;
+
+        while (number != 0) {
+            number >>>= 1;
+            count++;
+        }
+
+        return count;
+    }
+}
