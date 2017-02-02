51f1992315663586a79a2c7f2afa402244a7af2a
melalex <melalex490@gmail.com>
2017-02-02 21:35:10 +0200

    Before chunks inject

--- src/com/company/bitoperations/implementation/rsa/Chunks.java
+++ src/com/company/bitoperations/implementation/rsa/Chunks.java
@@ -21,29 +21,53 @@ public class Chunks {
 
     }
 
+    public byte[] getChunk(int index) {
+        byte[] chunk = chunks.get(index);
+        return Arrays.copyOf(chunk, chunk.length);
+    }
+
+    public byte getByte(int chunkNumber, int byteNumber) {
+        return chunks.get(chunkNumber)[byteNumber];
+    }
+
+    public int getByteCount() {
+        return chunks.stream().mapToInt(b -> b.length).reduce(0, (a, b) -> a + b);
+    }
+
     public static Chunks createChunks(@NotNull byte[] source, int chunkSize) {
         return createChunks(source, chunkSize, b -> b);
     }
 
     public static Chunks createChunks(@NotNull byte[] source,
                                       int chunkSize,
-                                      @NotNull Function<? super byte[], ? extends byte[]> function) {
+                                      @NotNull Function<? super byte[], ? extends byte[]> function) throws IllegalArgumentException{
         if (chunkSize <= 0)
             throw new IllegalArgumentException("chunkSize must be greater then 0. Got " + chunkSize);
 
         Chunks newChunk = new Chunks();
+        int sourceLength = source.length;
         int chunksCount = (source.length + chunkSize - 1) / chunkSize;
 
         newChunk.chunkSize = chunkSize;
         newChunk.chunks = IntStream.
                 range(0, chunksCount).
-                mapToObj(n -> function.apply(Arrays.copyOfRange(source, n * chunkSize, (n + 1) * chunkSize))).
+                mapToObj(n -> {
+                    byte[] result;
+
+                    if (sourceLength > (n + 1) * chunkSize) {
+                        result = Arrays.copyOfRange(source, n * chunkSize, (n + 1) * chunkSize);
+                    } else {
+                        result = Arrays.copyOfRange(source, n * chunkSize, sourceLength);
+                    }
+
+                    return function.apply(result);
+                }).
                 collect(Collectors.toList());
 
         return newChunk;
     }
 
-    public Chunks aplyFunction(@NotNull Function<? super byte[], ? extends byte[]> function) {
+    public Chunks applyFunction(@NotNull Function<? super byte[], ? extends byte[]> function) {
         Chunks result = new Chunks();
         LinkedList<byte[]> newChunks = new LinkedList<>();
         result.chunks = newChunks;
@@ -54,7 +78,7 @@ public class Chunks {
     }
 
     public byte[] asByteArray() {
-        ByteOutputStream stream = new ByteOutputStream();
+        ByteOutputStream stream = new ByteOutputStream(getByteCount());
         chunks.forEach(stream::write);
         return stream.getBytes();
     }
